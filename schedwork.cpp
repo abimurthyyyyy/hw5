94fff691362fbe1feeddfabeded8da9c65e47449
Abi Murthy <apmurthy@usc.edu>
2023-04-04 20:09:33 +0000

    backtrackign working

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool fillSched(
+    size_t row, 
+    DailySchedule& sched, 
+    const AvailabilityMatrix& avail, 
+    vector<size_t>& shifts, 
+    const size_t maxShifts,
+    const size_t dailyNeed
+    );
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +46,47 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    for (size_t i = 0; i < avail.size(); ++i) {
+        std::vector<Worker_T> tempRow;
+        sched.push_back(tempRow);
+    }
+    std::vector<size_t> shifts(avail[0].size());
 
+    if (fillSched(0,sched,avail,shifts, maxShifts, dailyNeed)){
+        // for (auto i:shifts){
+        //     cout << i << endl;
+        // }
+        return true;
+    }
+    
+    return false;
+    
 
+}
 
+bool fillSched(size_t row, DailySchedule& sched, const AvailabilityMatrix& avail, vector<size_t>& shifts, const size_t maxShifts,const size_t dailyNeed){
+    for (unsigned int i = 0; i < shifts.size(); ++i){
+        if (row == avail.size()){
+            return true;
+        }
+        if (avail[row][i] && shifts[i]<maxShifts && std::find(sched[row].begin(), sched[row].end(), i)==sched[row].end()){
+            shifts[i]++;
+            sched[row].push_back(i);
+            if (sched[row].size()==dailyNeed){
+                row++;
+            }
+            if (!fillSched(row,sched,avail,shifts, maxShifts, dailyNeed)){
+                shifts[i]--;
+                if (!sched[row].size()){
+                    row--;
+                }
+                sched[row].pop_back();
+            }
+            else{
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
